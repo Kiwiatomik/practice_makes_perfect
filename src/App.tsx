afec6f8c60eb8cf43e62fe2c913efc63f7e8b329
Kiwiatomik <thomas.favory@gmail.com>
2025-07-28 14:48:20 +0800

    Added Bootstrap dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { Container, Row, Col, Card } from 'react-bootstrap'
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <Container fluid className="App">
+      <Row className="justify-content-center mt-5">
+        <Col md={8} lg={6}>
+          <Card>
+            <Card.Body className="text-center">
+              <Card.Title as="h1">Practice Makes Perfect</Card.Title>
+              <Card.Text>
+                A learning platform that creates content that grows with you.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
